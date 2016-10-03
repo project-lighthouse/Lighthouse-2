5929d5a06c8d459968eaa4ea1f149b57dbe03061
David Teller <david.teller@univ-orleans.fr>
2016-10-03 16:52:46 +0200

    Simpler algorith, kind of seems to work. Also, writing to disk.

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def main():
 
     cap = cv2.VideoCapture(source)
     if cap is None or not cap.isOpened():
-        print('Warning: unable to open video source')
+        print('Error: unable to open video source')
         return -1
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
@@ -27,9 +27,6 @@ def main():
     backgroundSubstractor = cv2.createBackgroundSubtractorMOG2()
 
     # Record pixels where we have had most motion.
-    collector = None
-    prev = None
-    frame = None
     detecting = False
     detection_countdown = 0
     force_start = True
@@ -39,54 +36,42 @@ def main():
         ret, current = cap.read()
 
         key = cv2.waitKey(1) & 0xFF
+        # <q> or <Esc>: quit
         if key == 27 or key == ord('q'):
             break
+        # <spacebar> or `force_start`: start detecting.
         elif key == ord(' ') or force_start:
             force_start = False
             detecting = True
-            collector = numpy.zeros((HEIGHT, WIDTH), numpy.uint64)
             detection_countdown = FRAMES_TO_DETECT
 
         if not ret:
             # Somehow, we failed to capture the frame.
             continue
-        prev = frame
-        frame = current
+
+        # Display the current frame
+        cv2.imshow('frame', current)
+        cv2.moveWindow('frame', 0, 0)
 
         # Our operations on the frame come here
         if detecting:
-            motion = backgroundSubstractor.apply(frame) # FIXME: Is this the right subtraction?
-            positive = cv2.bitwise_and(motion, 255)
-#            negative = cv2.bitwise_and(motion, 127)
-            collector = collector + positive
+            foreground = backgroundSubstractor.apply(current) # FIXME: Is this the right subtraction?
+            positive = cv2.bitwise_and(foreground, 255)
             detection_countdown -= 1
+
+            cv2.imshow('tracking', positive)
+            cv2.moveWindow('tracking', WIDTH, HEIGHT)
+
+            gray = cv2.cvtColor(current, cv2.COLOR_BGR2GRAY)
+            tracking = cv2.bitwise_and(positive, gray)
+            cv2.imshow('objects', tracking)
+            cv2.moveWindow('objects', 0, HEIGHT)
+
+            cv2.imwrite('/tmp/tracking.png', tracking)
+
             if detection_countdown <= 0:
                 # Detection is over, time to extract/show the result.
                 detecting = False
-
-                # FIXME: We are only interested in pixels for which we have
-                # seen motion recently AND show up as active in collector.
-                threshold = numpy.percentile(collector, 75) # A bit too arbitrary for my tastes
-
-                # I'd prefer calling the faster cv2.threshold, but it doesn't like uint64
-                coll2 = numpy.zeros((HEIGHT, WIDTH), numpy.uint8)
-                display = numpy.zeros((HEIGHT, WIDTH, 3), numpy.uint8)
-                for y in range(0,HEIGHT):
-                    for x in range(0,WIDTH):
-                        if collector[y,x] > threshold:
-                            coll2[y,x] = 255
-                            display[y,x] = frame[y,x]
-
-                # Display where we have seen most motion during the countdown.
-                cv2.imshow('result', display)
-                cv2.imshow('collector', coll2)
-                cv2.imshow('motion', motion)
-
-
-
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
-
 
     # When everything done, release the capture
     cap.release()
