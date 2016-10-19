4386316972eb84966997a87a6da909cfd442d0dd
David Flanagan <dflanagan@mozilla.com>
2016-10-19 16:37:26 -0700

    Add audioutils.py module

---
+++ src/audioutils.py
@@ -0,0 +1,137 @@
+import math
+import time
+from array import array
+import alsaaudio
+
+BYTES_PER_SAMPLE = 2
+FORMAT = alsaaudio.PCM_FORMAT_S16_LE  # We use signed 16-bit samples
+SAMPLES_PER_SECOND = 16000            # at 16000 samples per second
+
+# Adjust this value as needed depending on mic sensitivity
+DEFAULT_SILENCE_THRESHOLD = 150
+
+# Generate a sine wave of the specified frequency and duration with
+# an amplitude that starts high and drops to zero. This does not actually
+# play a sound. It just returns an array that can be passed to audio.play()
+def makebeep(frequency, duration):
+    # Generate the waveform for this beep
+    samples = array('h')
+    numsamples = int(SAMPLES_PER_SECOND * duration)
+    samples_per_cycle = SAMPLES_PER_SECOND / frequency;
+    angle_per_sample = 2 * math.pi / samples_per_cycle;
+    phase = 0.0;
+    for i in range(0, numsamples):
+        factor = 1.0 - (float(i)/numsamples)
+        samples.append(int(factor * 32000 * math.sin(phase)))
+        phase = (phase + angle_per_sample) % (2*math.pi)
+
+    return samples
+
+start_recording_tone = makebeep(800, .2)
+stop_recording_tone = makebeep(400, .2)
+
+# Play the specified audio samples though the speakers.
+# This function expects and array or bytes object like those returned
+# by the makebeep() and record() functions.
+def play(samples):
+    if (len(samples) == 0):
+        return
+    speaker = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK)
+    speaker.setchannels(1)
+    speaker.setrate(SAMPLES_PER_SECOND)
+    speaker.setformat(FORMAT)
+    speaker.setperiodsize(len(samples)/BYTES_PER_SAMPLE)
+    start = time.time()
+    speaker.write(samples)
+    end = time.time()
+
+    # the write() call above only blocks until all the
+    # samples are buffered by the kernel, so it returns
+    # before the sound has finished playing.
+    # We want our function to block until the sound is done
+    duration = float(len(samples))/(BYTES_PER_SAMPLE * SAMPLES_PER_SECOND)
+    elapsed = end - start
+    time.sleep(duration - elapsed)
+
+def playfile(filename):
+    with open(filename, 'rb') as f:
+        play(f.read())
+
+
+# Record audio from the microphone, trim off leading and trailing silence
+# and return an array of the audio samples. If no sound is detected at all
+# then the returned array will have a length of zero.
+def record(min_duration=1,         # Record at least this many seconds
+           max_duration=8,         # But no more than this many seconds
+           max_silence=1,          # Stop recording after silence this long
+           silence_factor=0.1):    # Define "silence" as this fraction of max
+
+    chunk_duration = 1.0/16        # record in batches this many seconds long
+    chunk_size = int(SAMPLES_PER_SECOND * chunk_duration)
+    elapsed = 0.0                  # how many seconds recorded so far
+    elapsed_silence = 0.0          # how much silence at the end
+
+    # Chunks that are quieter than this count as silence
+    # We start with this default value, but may adjust up depending
+    # on how loud the user talks
+    silence_threshold = DEFAULT_SILENCE_THRESHOLD
+
+    # We're keep track of how loud the recording is, and start with an
+    # initial value chosen so that the silence threshold does not decrease
+    max_amplitude = int(silence_threshold / silence_factor)
+
+    mic = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
+    mic.setchannels(1)
+    mic.setrate(SAMPLES_PER_SECOND)
+    mic.setformat(FORMAT)
+    mic.setperiodsize(chunk_size)
+    recording = array('h')
+
+    while elapsed < max_duration:
+        l, chunk = mic.read()
+        chunkarray = array('h', chunk)
+        recording.extend(chunkarray)
+
+        chunk_duration = float(len(chunkarray)) / SAMPLES_PER_SECOND
+        elapsed += chunk_duration
+        chunk_max = max(chunkarray)
+
+        # Keep track of how loud the recording gets
+        if chunk_max > max_amplitude:
+            max_amplitude = chunk_max
+            silence_threshold = max_amplitude * silence_factor
+
+        # If this chunk is relatively quiet compared to the max
+        # then treat it as silence
+        if chunk_max < silence_threshold:
+            elapsed_silence += chunk_duration
+        else:
+            elapsed_silence = 0.0
+
+        # If we've recorded for at least the minimum time and have
+        # recorded at least the maximum silence, then stop recording.
+        if elapsed >= min_duration and elapsed_silence >= max_silence:
+            break
+
+    # trim the silence from the start and end of the recording
+    start = 0;
+    end = len(recording) - 1
+    while(start < len(recording) and recording[start] < silence_threshold):
+        start += 1
+    while(end > start and recording[end] < silence_threshold):
+        end -= 1
+    recording = recording[start:end+1]
+    return recording;
+
+if __name__ == '__main__':
+
+    play(start_recording_tone)
+    time.sleep(0.1)  # to ensure we don't pick up any of the tone in the mic
+    recording = record()
+    play(stop_recording_tone)
+
+    with open('/tmp/recording.raw', 'wb') as f:
+        recording.tofile(f)
+
+    time.sleep(.5)
+    playfile('/tmp/recording.raw')
